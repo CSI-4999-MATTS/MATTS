edaacb9a91c45095fa02fe261debe8c1c5455223
Broski-AC <acbroski@oakland.edu>
2020-09-28 19:36:29 -0400

    Adds firebase config

---
+++ src/firebase/config.js
@@ -0,0 +1,12 @@
+import * as firebase from "firebase";
+
+export const firebaseApp = firebase.initializeApp({
+    apiKey: process.env.REACT_APP_FRBS_API_KEY,
+    authDomain: process.env.REACT_APP_FRBS_AUTH_DOM,
+    databaseURL: process.env.REACT_APP_FRBS_DB_URL,
+    projectId: process.env.REACT_APP_FRBS_PROJ_ID,
+    storageBucket: process.env.REACT_APP_FRBS_STOR_BUCK,
+    messagingSenderId: process.env.REACT_APP_FRBS_MESS_ID,
+    appId: process.env.REACT_APP_FRBS_APP_ID,
+    measurementId: process.env.REACT_APP_FRBS_MEAS_ID
+});
